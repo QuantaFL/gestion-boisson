0679919112fdedb9833eeeda866ac9fbaf1efa4d
RBen19 <bonmerciel@gmail.com>
2025-07-06 01:32:58 +0000

    feat: implement ajouterBoisson on BoissonServiceImpl

--- src/main/java/org/gestion/boisson/features/boissons/services/impl/BoissonServiceImpl.java
+++ src/main/java/org/gestion/boisson/features/boissons/services/impl/BoissonServiceImpl.java
@@ -1,13 +1,30 @@
 package org.gestion.boisson.features.boissons.services.impl;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import lombok.extern.slf4j.Slf4j;
 import org.gestion.boisson.features.boissons.entities.Boisson;
+import org.gestion.boisson.features.boissons.repository.BoissonRepository;
 import org.gestion.boisson.features.boissons.services.BoissonService;
 
+@Named
+@ApplicationScoped
+@Slf4j
 public class BoissonServiceImpl implements BoissonService {
+    @Inject
+    private BoissonRepository boissonRepository;
 
     @Override
     public Boisson ajouterBoisson(Boisson boisson) {
-        return null;
+       try {
+           Boisson boisson1 =  boissonRepository.save(boisson);
+           log.info("Boisson enregistrée avec succès : {}", boisson);
+            return boisson1 ;
+       } catch (Exception e) {
+           log.error("Erreur lors de l'enregistrement de la boisson : {}", boisson, e);
+           throw e;
+       }
     }
 
     @Override
