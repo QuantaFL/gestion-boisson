204cb8320e56df7ffced2153a421735dcb5f1bcf
RBen19 <bonmerciel@gmail.com>
2025-07-06 03:25:12 +0000

    feat: delcare method for getAll on BoissonService

--- src/main/java/org/gestion/boisson/features/boissons/services/BoissonService.java
+++ src/main/java/org/gestion/boisson/features/boissons/services/BoissonService.java
@@ -2,10 +2,13 @@ package org.gestion.boisson.features.boissons.services;
 
 import org.gestion.boisson.features.boissons.entities.Boisson;
 
+import java.util.List;
+
 public interface BoissonService {
     Boisson ajouterBoisson(Boisson boisson);
     Boisson modifierBoisson(Boisson boisson);
     boolean supprimerBoisson(Long id);
     Boisson getBoissonDetails(String name);
     boolean boissonExists(String name);
+    List<Boisson> getAllBoissons();
 }
