6cc73bfa9fadc30c94084835e0d8e062593b8b19
RBen19 <bonmerciel@gmail.com>
2025-07-06 03:07:30 +0000

    feat; create controller for brevage ( boisson)

--- src/main/java/org/gestion/boisson/features/boissons/services/impl/BoissonServiceImpl.java
+++ src/main/java/org/gestion/boisson/features/boissons/services/impl/BoissonServiceImpl.java
@@ -19,7 +19,7 @@ public class BoissonServiceImpl implements BoissonService {
     public Boisson ajouterBoisson(Boisson boisson) {
        try {
            Boisson boisson1 =  boissonRepository.save(boisson);
-           log.info("Boisson enregistrée avec succès : {}", boisson);
+           log.info("Boisson  ajouté avec succès : {}", boisson);
             return boisson1 ;
        } catch (Exception e) {
            log.error("Erreur lors de l'enregistrement de la boisson : {}", boisson, e);
@@ -29,7 +29,14 @@ public class BoissonServiceImpl implements BoissonService {
 
     @Override
     public Boisson modifierBoisson(Boisson boisson) {
-        return null;
+        try {
+            Boisson boisson1 =  boissonRepository.save(boisson);
+            log.info("Boisson modifiée avec succès : {}", boisson);
+            return boisson1 ;
+        } catch (Exception e) {
+            log.error("Erreur lors de la modification  de la boisson : {}", boisson, e);
+            throw e;
+        }
     }
 
     @Override
@@ -39,11 +46,31 @@ public class BoissonServiceImpl implements BoissonService {
 
     @Override
     public Boisson getBoissonDetails(String name) {
-        return null;
+        try {
+            Boisson boisson = boissonRepository.getByNom(name);
+            if (boisson != null) {
+                log.info("Détails de la boisson récupérés avec succès : {}", boisson);
+                return boisson;
+            } else {
+                log.warn("Aucune boisson trouvée avec le nom : {}", name);
+                return null;
+            }
+        } catch (Exception e) {
+            log.error("Erreur lors de la récupération des détails de la boisson : {}", name, e);
+            throw e;
+        }
     }
 
     @Override
     public boolean boissonExists(String name) {
-        return false;
+        try {
+            Boisson boisson = boissonRepository.getByNom(name);
+            boolean exists = boisson != null;
+            log.info("Vérification de l'existence de la boisson '{}': {}", name, exists);
+            return exists;
+        } catch (Exception e) {
+            log.error("Erreur lors de la vérification de l'existence de la boisson : {}", name, e);
+            throw e;
+        }
     }
 }
