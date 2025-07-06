0b8d6c70b3c329083a83f4c7112dbb95634a6c11
RBen19 <bonmerciel@gmail.com>
2025-07-06 03:27:37 +0000

    feat: implement getAll with Mappers on BoissonController

--- src/main/java/org/gestion/boisson/features/boissons/controller/BoissonController.java
+++ src/main/java/org/gestion/boisson/features/boissons/controller/BoissonController.java
@@ -8,6 +8,7 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import lombok.extern.slf4j.Slf4j;
 import org.gestion.boisson.features.boissons.mappers.BoissonMapper;
 import org.gestion.boisson.features.boissons.services.BoissonService;
 
@@ -15,6 +16,7 @@ import org.gestion.boisson.features.boissons.services.BoissonService;
 @Path("/boissons")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@Slf4j
 public class BoissonController {
     @Inject
     private BoissonService boissonService;
@@ -24,5 +26,18 @@ public class BoissonController {
     @Path("/health")
     public Response healthCheck() {
         return Response.ok("Service is running").build();
+    }
+    @GET
+    @Path("")
+    public Response getBoissons() {
+        try {
+            log.info("Fetching all boissons");
+            return Response.ok(boissonMapper.toDtoList(boissonService.getAllBoissons())).build();
+
+        } catch (Exception e) {
+            log.error("Error fetching boissons: {}", e.getMessage(), e);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("Error fetching boissons: " + e.getMessage()).build();
+        }
     }
 }
