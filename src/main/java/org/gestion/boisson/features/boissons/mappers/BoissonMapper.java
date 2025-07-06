a7438a8cc0089b095a82dcb2fd3abed27091f465
RBen19 <bonmerciel@gmail.com>
2025-07-06 03:26:27 +0000

    feat: create mapper interface

--- src/main/java/org/gestion/boisson/features/boissons/mappers/BoissonMapper.java
+++ src/main/java/org/gestion/boisson/features/boissons/mappers/BoissonMapper.java
@@ -3,7 +3,11 @@ package org.gestion.boisson.features.boissons.mappers;
 import org.gestion.boisson.features.boissons.dto.BoissonDto;
 import org.gestion.boisson.features.boissons.entities.Boisson;
 
+import java.util.List;
+
 public interface BoissonMapper {
     Boisson toEntity(BoissonDto dto);
     BoissonDto toDto(Boisson entity);
+    List<BoissonDto> toDtoList(List<Boisson> boissons);
+    List<Boisson> toEntityList(List<BoissonDto> dtos);
 }
